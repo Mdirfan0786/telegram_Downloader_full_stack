aed6757229fdce853002eda23173159855e9f59a
MD IRFAN <abc@gmail.com>
2026-05-31 16:09:51 +0530

    fix: remove global Telegram client from server startup

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 
 import app from "./app.js";
 
-import { client } from "./config/telegram.js";
-
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -12,18 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 async function startServer() {
   try {
-    // Connect MongoDB
     await connectDB();
 
-    // Connect Telegram
-    await client.connect();
-
-    // Verify Telegram auth
-    const me = await client.getMe();
-
-    console.log(`Telegram Connected: ${me.firstName}`);
-
-    // Start Express server
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
